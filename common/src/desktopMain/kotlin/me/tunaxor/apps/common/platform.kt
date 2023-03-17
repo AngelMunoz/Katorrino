ae82474d726c5ee7638f5e41c7c6ded0732588c3
Angel D. Munoz <scythes.lol@gmail.com>
2023-03-16 23:09:40 -0600

    implement file chooser for desktop

--- common/src/desktopMain/kotlin/me/tunaxor/apps/common/platform.kt
+++ common/src/desktopMain/kotlin/me/tunaxor/apps/common/platform.kt
@@ -1,5 +1,26 @@
 package me.tunaxor.apps.common
 
+import javax.swing.JFileChooser
+import javax.swing.filechooser.FileFilter
+import javax.swing.filechooser.FileNameExtensionFilter
+import javax.swing.filechooser.FileSystemView
+
 actual fun getPlatformName(): String {
     return "Desktop"
+}
+
+actual fun getFiles(): List<Song> {
+    val directory = FileSystemView.getFileSystemView().homeDirectory
+
+    val dialog = JFileChooser(directory).apply {
+        dialogTitle = "Select Your Music Files"
+        isMultiSelectionEnabled = true
+        fileFilter =
+            FileNameExtensionFilter("Only MP3 Files", "mp3")
+    }
+    when (dialog.showOpenDialog(null)) {
+        JFileChooser.APPROVE_OPTION ->
+            return dialog.selectedFiles.map { Song(it.name, it.path) }
+    }
+    return emptyList()
 }
