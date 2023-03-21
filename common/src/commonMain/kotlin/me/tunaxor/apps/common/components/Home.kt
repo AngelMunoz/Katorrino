e2dbf0d8ce23927029365e288afb541077317976
Angel D. Munoz <scythes.lol@gmail.com>
2023-03-20 21:29:10 -0600

    update code to properly select songs

--- common/src/commonMain/kotlin/me/tunaxor/apps/common/components/Home.kt
+++ common/src/commonMain/kotlin/me/tunaxor/apps/common/components/Home.kt
@@ -4,12 +4,9 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.material.Button
-import androidx.compose.material.ListItem
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import com.arkivanov.decompose.ComponentContext
@@ -18,14 +15,14 @@ import com.arkivanov.decompose.value.MutableValue
 import com.arkivanov.decompose.value.Value
 import me.tunaxor.apps.common.RouteConfig
 import me.tunaxor.apps.common.Song
-import me.tunaxor.apps.common.getFiles
+import me.tunaxor.apps.common.SongFilePicker
 
 interface HomeComponent {
 
     val songs: Value<List<Song>>
     fun navigateToPage(routeConfig: RouteConfig)
 
-    fun getSongs()
+    fun setSongs(foundSongs: List<Song>)
 }
 
 class DefaultHomeComponent(
@@ -36,33 +33,31 @@ class DefaultHomeComponent(
 
     override val songs = MutableValue(emptyList<Song>())
 
-    override fun getSongs() {
-        songs.value = getFiles()
+    override fun setSongs(foundSongs: List<Song>) {
+        songs.value = foundSongs
     }
+
     override fun navigateToPage(routeConfig: RouteConfig) {
         goToPage(routeConfig)
     }
+
 }
 
 @Composable
 fun HomeContent(component: HomeComponent) {
     val songs by component.songs.subscribeAsState()
-    Column(modifier= Modifier.padding(12.dp)) {
+    Column(modifier = Modifier.padding(12.dp)) {
+        SongFilePicker(onSelectSongs = { component.setSongs(it) })
         when (songs.isEmpty()) {
             true ->
                 Text("No Songs Selected")
+
             else ->
                 LazyColumn {
                     items(songs, key = { it.name }) {
                         Text(it.name)
                     }
                 }
-        }
-
-        Button(onClick = {
-            component.getSongs()
-        }) {
-            Text("Load Songs")
         }
     }
 
